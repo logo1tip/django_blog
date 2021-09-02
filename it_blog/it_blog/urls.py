8ee3325157ea2a878b8d1a4690e44be9948e9455
logo1tip <evgeni.leschinski@gmail.com>
2021-09-02 22:04:47 +0300

    made templates

--- it_blog/it_blog/urls.py
+++ it_blog/it_blog/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.http import request
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("home/", include("home.urls")),
-    path("", lambda request: redirect("/home/"))
-]
+    path("^$", lambda request: redirect("/home/")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
